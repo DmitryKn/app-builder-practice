319e1e3da9bc3e995ba1f583744979f3153b276b
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-03-28 11:43:48 -0400

    8 Order buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Aux from '../../../hoc/ReactAux.js';
+import Button from '../../UI/Button/Button.js';
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey => {
       return (
-        <li key={igKey} style={{listStyle: 'none'}}>
+        <li key={igKey} style={{listStyle: 'none', textAlign: 'left'}}>
           <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
        </li>);
     });
 
   return (
     <Aux>
-      <h3>Your Order</h3>
+      <h3>Your Order </h3>
       <p> A delicious burger with the following ingredients: </p>
       <ul>
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout?</p>
+      <Button btnType={"Danger"} clicked={props.purchaseCanceled}>CANCEL</Button>
+      <Button btnType={"Success"} clicked={props.purchaseContinued}>CONTINUE</Button>
+
     </Aux>
   )
 };
