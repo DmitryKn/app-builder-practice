38f6ec6ececf60c2c7bd7f2e640b167c26572f37
Dmitry Knyazev <4dmitry.kn@gmail.com>
2018-03-29 14:41:35 -0400

    9.3 Toolbar SideDrawer + responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/ReactAux';
 import './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar.js';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer.js';
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <main className="Content">
-      {props.children}
-    </main>
-</Aux>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
 
-export default layout;
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false})
+  }
+
+  render () {
+    return (
+      <Aux>
+        <Toolbar />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}/>
+        <main className="Content">
+          {this.props.children}
+        </main>
+      </Aux>
+    );
+  }
+} //end
+export default Layout;
